2c8bac0d4b983de269ba258dc880be5117c39073
ss28467 <SunkaraShashankRedd@JohnDeere.com>
2018-12-24 12:46:24 -0600

    Thread.sleep=30000

--- src/main/java/com/example/hello/christmas/ChristmasApplication.java
+++ src/main/java/com/example/hello/christmas/ChristmasApplication.java
@@ -13,7 +13,7 @@ public class ChristmasApplication {
 
 	public static void main(String[] args) {
 		try {
-			Thread.sleep(40000);
+			Thread.sleep(30000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
