b210a06be8090bf3eea69000e8346890e3d08b5b
Sunny <inusasunny@gmail.com>
2018-12-24 14:19:44 -0600

    Update ChristmasApplication.java

--- src/main/java/com/example/hello/christmas/ChristmasApplication.java
+++ src/main/java/com/example/hello/christmas/ChristmasApplication.java
@@ -13,7 +13,7 @@ public class ChristmasApplication {
 
 	public static void main(String[] args) {
 		try {
-			Thread.sleep(31000);
+			Thread.sleep(30000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
